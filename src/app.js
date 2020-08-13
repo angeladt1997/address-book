60915f85b19b853bdef9f3e193216976f1cf3254
Angela Thomas <angeladthomas97@gmail.com>
2020-08-13 16:30:31 -0500

    At least we aren't fwe aren't failing anymore...that's something right?

--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
 const { v4: uuid } = require('uuid')
+const API_TOKEN = process.env.API_TOKEN
+
+
 
 const app = express()
 
@@ -13,75 +16,44 @@ const morganOption = (NODE_ENV === 'production')
   : 'common';
 
 app.use(morgan(morganOption))
-app.use(cors())
 app.use(helmet())
+app.use(cors())
 app.use(express.json())
 
-function validateBearerToken(req, res, next){
-  const apiToken = process.env.API_TOKEN;
-  const authToken = req.get('authorisation')
-  if(!authToken || authToken.split('')[1] !==apiToken){
-    return res.status(401).json({error:'unauthorised request'})
+// validate auth Token in request, must match token in .env
+function bearerTokenValidation(req, res, next) {
+  const authHeader = req.get('Authorization')
+  const token = authHeader.split(' ')[1]
+
+  if(token !== API_TOKEN) {
+    return res
+      .status(401)
+      .send('Unauthorized request')
   }
-  next ();
+  next()
 }
 
-const address= [{
-  "id": "3c8da4d5-1597-46e7-baa1-e402aed70d80",
-  "firstName": "sallyStudent",
-  "lastName": "c00d1ng1sc00l",
-  "address1": "Cache Valley Stone Society",
-  "address2": "false",
-  "city": "yes",
-  "state": "mhm",
-  "zip": 12345
+const addressBook = [
+  {
+    'id': uuid(),
+    'firstName': 'Bob',
+    'lastName': 'Test',
+    'address1': '123 Main Street',
+    'address2': 'apt 1',
+    'city': 'SLC',
+    'state': 'UT',
+    'zip': 84123
+  }
+]
 
-}]
-
-	app.get('/address', (req, res) => {
-		res.json(address); 
-	})
-
-  app.post('/address', validateBearerToken)
-  //   res.send('POST Request recieved');
-  //    const data = {
-  //     user: "username",
-  //     pw: "password"
-  //   };
-  //   fetch('http://localhost: 8000/auth/login', {
-  //     method: "POST",
-  //     headers: {
-  //       "Content-Type": "application/json"
-  //     }
-  //     body: JSON.stringify(date)
-  //   })
-  //   .then(...)
-  // })
-
-  app.delete('/address/userId', validateBearerToken)
-
-
-app.use(function errorHandler(error, req, res, next) {
-   let response
-   if (NODE_ENV === 'production') {
-     response = { error: { message: 'server error' } }
-   } else {
-     console.error(error)
-     response = { message: error.message, error }
-   }
-   res.status(500).json(response)
+app.get('/address', (req, res) => {
+  res.status(200).json(addressBook)
 })
 
-
-
-
-
-
-
-app.post('/address', (req, res) => {
-  const {firstName, lastName, address1, address2 = false, city, state, zip}=req.body;
- 
-  if (!address) {
+app.post('/address', bearerTokenValidation, (req, res) => {
+  const { firstName, lastName, address1, address2, city, state, zip } = req.body
+  
+   if (!address) {
     return res
     .status(400)
     .send('Did you...did you skip the whole form?')
@@ -145,10 +117,8 @@ if (!lastName) {
     .send('Ian ask for yo number, I asked for yo zip code. Lemme get that');
   }
 
-})
-
-const id = uuid();
-const newAddress = {
+  const id = uuid()
+  const newAddress = {
     id,
     firstName,
     lastName,
@@ -157,27 +127,160 @@ const newAddress = {
     city,
     state,
     zip
-
   }
 
-adress.push(newAddress);
-res
-.status(200)
-.send('You did that right. So proud of you')
+  addressBook.push(newAddress)
 
-  app.delete('/adress/:id', (req, res) => {
-  const { userId } = req.params;
+  res
+    .status(201)
+    .location(`http://localhost:8000/address/${id}`)
+    .json(newAddress)
+})
 
-  const index = adress.findIndex(u => address.id === id);
-  if (index === -1) {
-    return res
-      .status(404)
-      .end();
+app.delete('/address/:id', bearerTokenValidation, (req, res) => {
+  const { id } = req.params
+  const addressIndex = addressBook.findIndex(item => item.id === id)
+
+  addressBook.splice(addressIndex, 1)
+
+  res
+    .status(204)
+    .end()
+})
+
+app.use(function errorHandler(error, req, res, next) {
+  let response
+  if(NODE_ENV === 'production') {
+    response = { error: {message: 'server error'}}
+  } else {
+    console.error(error)
+    response = { message: error.message, error }
   }
-
-  users.splice(index, 1);
-
-  res.send('Deleted');
-});
+  res.status(500).json(response)
+})
 
 module.exports = app
+
+// require('dotenv').config()
+// const express = require('express')
+// const morgan = require('morgan')
+// const cors = require('cors')
+// const helmet = require('helmet')
+// const { NODE_ENV } = require('./config')
+// const { v4: uuid } = require('uuid')
+
+// const app = express()
+
+// const morganOption = (NODE_ENV === 'production')
+//   ? 'tiny'
+//   : 'common';
+
+// app.use(morgan(morganOption))
+// app.use(cors())
+// app.use(helmet())
+// app.use(express.json())
+
+// function validateBearerToken(req, res, next){
+//   const apiToken = process.env.API_TOKEN;
+//   const authToken = req.get('authorisation')
+//   if(!authToken || authToken.split('')[1] !==apiToken){
+//     return res.status(401).json({error:'unauthorized request'})
+//   }
+//   next ();
+// }
+
+//  const address= [{
+//   "id": "3c8da4d5-1597-46e7-baa1-e402aed70d80",
+//   "firstName": "sallyStudent",
+//   "lastName": "c00d1ng1sc00l",
+//   "address1": "Cache Valley Stone Society",
+//   "address2": "false",
+//   "city": "yes",
+//   "state": "mhm",
+//   "zip": 12345
+
+// }]
+
+
+
+// 	app.get('/address', (req, res) => {
+// 		res.json(address); 
+// 	})
+
+  
+//   //   res.send('POST Request recieved');
+//   //    const data = {
+//   //     user: "username",
+//   //     pw: "password"
+//   //   };
+//   //   fetch('http://localhost: 8000/auth/login', {
+//   //     method: "POST",
+//   //     headers: {
+//   //       "Content-Type": "application/json"
+//   //     }
+//   //     body: JSON.stringify(date)
+//   //   })
+//   //   .then(...)
+//   // })
+
+//   app.delete('/address/addressId', validateBearerToken)
+
+
+// app.use(function errorHandler(error, req, res, next) {
+//    let response
+//    if (NODE_ENV === 'production') {
+//      response = { error: { message: 'server error' } }
+//    } else {
+//      console.error(error)
+//      response = { message: error.message, error }
+//    }
+//    res.status(500).json(response)
+// })
+
+
+
+
+
+
+
+// app.post('/address', validateBearerToken (req, res) => {
+
+//    const {firstName, lastName, address1, address2 = false, city, state, zip}=req.body;
+ 
+
+
+// const id = uuid();
+// const newAddress = {
+//     id,
+//     firstName,
+//     lastName,
+//     address1,
+//     address2,
+//     city,
+//     state,
+//     zip
+
+//   }
+
+// address.push(newAddress);
+// res
+// .status(200)
+// .location(`http://localhost:8000/address/${id}`)
+// .send('You did that right. So proud of you')
+
+//   app.delete('/adress/:id', (req, res) => {
+//   const { userId } = req.params;
+
+//   const index = address.findIndex(u => address.id === id);
+//   if (index === -1) {
+//     return res
+//       .status(404)
+//       .end();
+//   }
+
+//   address.splice(index, 1);
+
+//   res.send('Deleted');
+// });
+
+// module.exports = app
--- test/app.spec.js
+++ test/app.spec.js
@@ -2,9 +2,9 @@
 const app = require('../src/app')
 
 describe('App', () => {
-  it('GET / responds with 200 containing address', () => {
+  it('GET / responds with 200 containing "Hello, world!', () => {
     return supertest(app)
       .get('/')
-      .expect(200, "address")
+      .expect(200, "Hello, world!")
   })
 })
